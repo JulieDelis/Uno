4f512cb40c5ab1fe642de36afb80fc0935c2b657
sirius26110 <sirius26110@gmail.com>
2022-08-31 19:43:27 +0200

    début de la Documentation (V1)

--- cartes.py
+++ cartes.py
@@ -4,11 +4,17 @@ class Carte:
         self.symbole = symbole  
     
     def montrer(self):
+        """
+        Permet de montré le contenue d’instances contenant des carte
+        ATENTION cette fonction ne retourne rien, si utiliser avec print, valeur affichée = None
+        """
         print((self.couleur, self.symbole))
     # ! cette fonction ne retourne rien, si utliser avec print, valeur affichée = None
     
     def peut_poser(self, carte_sur_le_tas):
-        
+        """
+        Permet de retourner une valeur booléenne selon si la carte peut être poser 
+        """
         if carte_sur_le_tas.couleur == self.couleur:
             return True
         # règles aditionelle ici
