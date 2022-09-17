641f1fb0bbfa61673828ad070ce6b924db085c80
Julie <juliedelis@icloud.com>
2022-09-17 10:44:09 +0200

    Jeu interactif provisoire, necessite une Màj de l'affichage pour fonctionner

--- fonctions.py
+++ fonctions.py
@@ -2,18 +2,32 @@ import cartes
 import random
 
 def genere_cartes(couleurs, symboles, nb_joueurs):
+
+    """ Génère les cartes.
+    arguments:
+        couleurs: liste des couleurs attribuées aux cartes, servira dans la génération, la fonctions génère un certains nombre n de cartes pour chaque couleur.
+        symboles: liste des symboles qui doivent apparaitre sur les cartes, pour l'insant on attend la liste des nombres de 0 à 9 (compris) mais cela sera ammené à changer avec l'ajout de nouveaux
+        symboles.
+        nb_joueurs: nombre de joueur qui influence le nombre de paquets généré, si le nombre de joueur est trop élévé alors la fonction générera un paquet de cartes en plus
+
+    Cette fonctions retourne une liste d'objets cartes dans leur ordre de génération.
+        """
+    
     jeu = []
     nb_jeu = nb_joueurs * 7 + nb_joueurs + 1 # 1 pour le tas et une carte de pioche chaque joueur
     for joueur in range(nb_joueurs % 40): # 40 ou nombre de cartes dans un jeu
         for couleur in couleurs:
             for symbole in symboles:
-            # ici pour symbole dans feneration if symbol == id du symbol mettre 2 cartes de ça
+            # ici pour symbole dans generation if symbol == id du symbol mettre 2 cartes de ça
                 carte = cartes.Carte(couleur, symbole)
                 jeu.append(carte)
     return jeu
 
 
 def melange(pioche):
+
+    """Mélange les éléments de la liste donnée en argument, retourne une nouvelle liste."""
+
     indices_deja_passe = []
     nouvelle_pioche = []
     while len(nouvelle_pioche) < len(pioche):
@@ -27,9 +41,16 @@ def melange(pioche):
         indices_deja_passe.append(indice_aleatoire)
     return nouvelle_pioche
 
+
+
 def distribue(cartes, nb_joueur):
-     # liste de liste instance pour pioche, tas , joueurs
-    """ Retourne une liste d'instances de jeu contenant les cartes dans l'ordre suivant, joueurs, pioche, tas."""
+
+    """Attribue les cartes d'une liste à trois instances: la piohce, le tas, et les joueurs.
+arguments:
+    cartes: liste des cartes à distribuer entre les trois instances
+    nb_joueur: nombre de joueur auquels on devra attribuer chacun 7 cartes
+Retourne une liste d'instances de jeu, c'est à dire de listes, contenant les cartes dans l'ordre suivant, joueurs, pioche, tas."""
+
     pioche = cartes
     tas = [pioche.pop(0)]
     # s'assurer que on tape pas 1
@@ -44,14 +65,31 @@ def distribue(cartes, nb_joueur):
     
     return instances
 
+
+
 def preparation(nb_joueurs):
+
+    """ Prépare le jeu le paquets de cartes pour le début du jeu.
+    arguments: 
+        nb_joueurs: nombre de joueurs (pour l'instant il n'est question de bots) 
+    Retourne une liste de listes (instances de jeu) d'objets carte."""
+
     couleurs = ["bleu", "rouge", "jaune", "vert"]
     symboles = range(10)
 
     instances = distribue(melange(genere_cartes(couleurs, symboles, nb_joueurs)), nb_joueurs)
     return instances
 
-def joue(joueur, pioche, tas): # un joueur = liste de cartes = cartes ici
+def joue(joueur, pioche, tas):
+    
+    """ Joue pour un bot.
+    arguments: 
+        joueur: liste des cartes d'un joueur
+        pioche: liste ordonnée des cartes contenus dans la pioche au moment du tour du joueur
+        tas: carte sur le tas au moment du tour du joueur
+    Retourne une nouvelle instance de chacun des arguments (c-à-d une nouvelle liste de cartes) en fonction des modifications faites au cour du tour du joueur."""
+
+     # un joueur = liste de cartes = cartes ici
     cartes_jouables = [] # indices des cartes
     coulleur_de_baze = '\033[0m' # Défini la couleur en blanc sinon la couleur des textes continue d’être de la couleur de la dernière carte  
     print(coulleur_de_baze, "---------------")
@@ -76,7 +114,40 @@ def joue(joueur, pioche, tas): # un joueur = liste de cartes = cartes ici
     print(coulleur_de_baze, "-----")
     return joueur, pioche, tas
 
+def joue_joueur(joueur, pioche, tas):
 
+    cartes_jouables = [] # indices des cartes
+    coulleur_de_baze = '\033[0m' # Défini la couleur en blanc sinon la couleur des textes continue d’être de la couleur de la dernière carte 
+    print(coulleur_de_baze, "---------------")
+    print("carte de joueur : ")
+
+    for carte in joueur:
+        carte.montrer()   
+    print(coulleur_de_baze,"-----")
+
+    for carte_id, carte in enumerate(joueur):
+        # print(tas[-1].montrer())
+        if carte.peut_poser(tas[-1]):
+            cartes_jouables.append(carte_id)
+            
+    # print(cartes_jouables)
+    print(coulleur_de_baze, "Le tas est : ")
+    tas[-1].montrer()
+
+    if cartes_jouables == []:
+        print("Vous piochez.")
+        joueur.append(pioche.pop(0))
+    else:
+        carte_id = int(input("Choisissez l'id de la carte à jouer"))
+        if carte_id not in cartes_jouables:
+            print("La carte n'est pas jouable!")
+            carte_id = cartes_jouables[random.randint(0, len(cartes_jouables)-1)]    
+        print(coulleur_de_baze, "Vous jouez la carte : ")  
+        joueur[carte_id].montrer()
+        tas.append(joueur.pop(carte_id))
+    print(coulleur_de_baze, "-----")
+    return joueur, pioche, tas
+# à vérifier avec l'affichage, problèmes 
 def pioche_vide(pioche, tas):
     if pioche == []:
         nouvelle_pioche = melange(tas[:-1])
@@ -102,4 +173,6 @@ def jeu():
                 print(num, "gagné")
                 fin_du_jeu = True
                 break
+            if num == 0: # temporaire, juste pour choisir quel joueur est vraiment joueur et le differenttier des bots, enlever cette ligne fait joueur les bots entre eux
+                joueur, pioche, tas = joue_joueur(joueur, pioche, tas)
             joueur, pioche, tas = joue(joueur, pioche, tas)
